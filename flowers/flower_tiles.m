783840e58fb88dfc32f02d8e25fb9985c904cdfd
Eric Ludlam <eludlam@mathworks.com>
2025-06-23 21:39:16 +0100

    Add waterlily to the tiles.
    Remove interactions from axes in the figure.

--- flowers/flower_tiles.m
+++ flowers/flower_tiles.m
@@ -2,10 +2,10 @@ function flower_tiles
 % Display the interesting pumpkins in a tiled layout.
 
     fig = gcf;
-    fig.Position(3:4) = [ 1000 300 ];
+    fig.Position(3:4) = [ 1500 300 ];
     fig.Color = 'w';
     
-    tiledlayout('flow','TileSpacing','none','Padding','none');
+    tiledlayout('horizontal','TileSpacing','none','Padding','none');
 
     nexttile;
     redrose
@@ -13,6 +13,7 @@ function flower_tiles
     set(gca,'clipping','off')
     axis off
     title('Rose')
+    configaxis
 
     nexttile;
     dahlia
@@ -20,14 +21,36 @@ function flower_tiles
     set(gca,'clipping','off')
     axis off
     title('Dahlia')
+    configaxis
 
     nexttile
     daffodil
     axis off
     title('Daffodil');
+    configaxis
 
     nexttile
     tulip
     axis off
     title('Tulip');
+    configaxis
+
+    nexttile
+    waterlily
+    title('Water Lily');
+    set(gca,'clipping','off')
+    axis([-.7 .7 -.7 .7 0 .8],'off')
+    configaxis
+
 end
+
+function configaxis()
+    io = get(gca,'InteractionOptions');
+    io.PanSupported = false;
+    io.ZoomSupported = false;
+    io.DatatipsSupported = false;
+    io.BrushSupported = false;
+    set(gca,'InteractionOptions',io);
+    axtoolbar(gca,{ 'restoreview' });
+end
+            
